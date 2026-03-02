7dc5696ac6ddcda2da8a535dd504ab8f5f25a3bb
PolaCamacho <paulaelviracamacho@gmail.com>
2026-03-02 20:24:16 -0300

    1

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/Reutilizables";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
+      <header>
+      <NavBar />
+
+      </header>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
