3be98e1862e301096a314dee6c9d63dd29583eef
Cristhian Castro <cristhiancastro204@gmail.com>
2026-03-04 21:15:43 -0300

    merge main into cris

--- api/api.js
+++ api/api.js
@@ -1,13 +1,5 @@
 const URL = "https://api-react-taller-production.up.railway.app";
 
-<<<<<<< HEAD
-const register = async (username , name , password  ) =>{
-
-    const response = await fetch(`${URL}/api/auth/register` , {
-        method: "POST",
-        headers:{"Content-Type": "application/json",},
-        body: JSON.stringify({username, name, password})
-=======
 const register = async (username, name, password) => {
   const response = await fetch(`${URL}/api/auth/register`, {
     method: "POST",
@@ -46,37 +38,10 @@ const postLocal = async (name, type, priceRange , city, zone, address , hours , 
             "Authorization" : `Bearer ${localStorage.getItem("token")}`
         },
         body: JSON.stringify({name, type, priceRange , city, zone, address , hours , photos})
->>>>>>> main
     });
 
     const data = await response.json();
 
-<<<<<<< HEAD
-    console.log("Informacion de Registro" , data);
-}
-
-const login = async (username , password  ) =>{
-
-    const response = await fetch(`${URL}/api/auth/login` , {
-        method: "POST",
-        headers:{"Content-Type": "application/json",},
-        body: JSON.stringify({username, password})
-    });
-
-    const data = await response.json();
-
-    localStorage.setItem("token", data.token);
-    localStorage.setItem("user", JSON.stringify(data.user));
-
-    return data;
-}
-
-
-
-export{
-    register,
-    login,
-=======
     console.log("Informacion del Local Creado", data);
 
 }
@@ -85,5 +50,4 @@ export{
     register,
     login,
     postLocal,
->>>>>>> main
 }
